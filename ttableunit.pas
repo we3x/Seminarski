5d9fd33efcf9735b5d013b2fd0e1ad974c237b4d
Veljko Simic <veljko.simic98@gmail.com>
2015-06-02 22:06:16 +0200

    Add cells record

--- ttableunit.pas
+++ ttableunit.pas
@@ -12,7 +12,12 @@ const
   NumOfRow = 7;      {Broj Redova}
 
 type
-  matShapes = array[1..NumOfColumn] of array[1..NumOfRow] of integer;
+  cells = record
+    value : integer;
+    shape: TShape;
+  end;
+
+  matShapes = array[1..NumOfColumn] of array[1..NumOfRow] of cells;
   TTable = class
   private
          table :matShapes;
@@ -21,27 +26,27 @@ type
          function isLegalMove(a,b:integer):boolean;
          function winner():integer;
          procedure move(player,a,b:integer);
-
+  end;
 implementation
-         procedure newBoard();
+         procedure TTable.newBoard();
          {kreira praznu tablu, u matricu postavlja sve nule}
          begin
          end;
 
-         function isLegalMove(a,b:integer):boolean;
+         function TTable.isLegalMove(a,b:integer):boolean;
          {funkciji se prosledjuju koordinate polja koje igrac zeli da odigra
          ukoliko je to polje legalno za potez funkcija vraca true, u suprotnom
          vraca false}
          begin
          end;
 
-         function winner():integer;
+         function TTable.winner():integer;
          {funkcija prolazi kroz matricu i ukoliko postoji pobednik vraca jedan
          ili dva, ukoliko nema pobednika vraca -1}
          begin
          end;
 
-         procedure move(player,a,b:integer);
+         procedure TTable.move(player,a,b:integer);
          {procedura u matricu upisuje na polje kordinata(a,b) redni broj igraca
          1 ili 2 to se smatra potezom}
          begin
